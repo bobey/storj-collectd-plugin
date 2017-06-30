85bc0e9a4bb702ddf6c203da04db0e25eac520c6
Olivier Balais <obalais@gmail.com>
2017-06-30 07:54:58 +0200

    Now handle contracts, shared in percents and delta values

--- index.js
+++ index.js
@@ -8,9 +8,29 @@ const interval = process.env.COLLECTD_INTERVAL || 120;
 const storjDaemonPort = 45015;
 
 const outputCollectdData = (share) => {
-    console.log(`PUTVAL ${host}/storj/peers-${share.id} interval=${interval} N:${share.meta.farmerState.totalPeers}`);
-    console.log(`PUTVAL ${host}/storj/restarts-${share.id} interval=${interval} N:${share.meta.numRestarts}`);
-    console.log(`PUTVAL ${host}/storj/shared-${share.id} interval=${interval} N:${share.meta.farmerState.spaceUsedBytes}`);
+    const totalPeers = share.meta.farmerState.totalPeers || 0;
+    const numRestarts = share.meta.numRestarts || 0;
+    const spaceUsedBytes = share.meta.farmerState.spaceUsedBytes || 0;
+    const contractCount = share.meta.farmerState.contractCount || 0;
+    const delta = fixDeltaValue(share);
+    const percentUsed = share.meta.farmerState.percentUsed || 0;
+
+    console.log(`PUTVAL ${host}/storj/peers-${share.id} interval=${interval} N:${totalPeers}`);
+    console.log(`PUTVAL ${host}/storj/restarts-${share.id} interval=${interval} N:${numRestarts}`);
+    console.log(`PUTVAL ${host}/storj/shared-${share.id} interval=${interval} N:${spaceUsedBytes}`);
+    console.log(`PUTVAL ${host}/storj/contracts-${share.id} interval=${interval} N:${contractCount}`);
+    console.log(`PUTVAL ${host}/storj/delta-${share.id} interval=${interval} N:${delta}`);
+    console.log(`PUTVAL ${host}/storj/percentused-${share.id} interval=${interval} N:${percentUsed}`);
+};
+
+const fixDeltaValue = (share) => {
+    let delta = share.meta.farmerState.ntpStatus.delta;
+
+    if (!delta) {
+        return 0;
+    }
+
+    return parseInt(delta.replace('ms', ''), 10);
 };
 
 const status = () => {
